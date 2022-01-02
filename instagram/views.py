9e9a2bdf4c3658623c1c75cab63ddbfc27e695b0
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2022-01-02 22:28:17 +0900

    dispatch

--- instagram/views.py
+++ instagram/views.py
@@ -5,7 +5,16 @@ from .models import Post
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all() # 데이터의 범위 지정, 로직마다 달라질 경우 get_queryset 사용가능
     serializer_class = PostSerializer
-
     # 이것만으로 post_list의 2개 분기, post_detail의 3개 분기를 지원한다. -> url이 2개 필요
-    #
+
+    # request의 method에 맞는 함수를 호출하는 함수
+    # ex) request.method가 GET이면 get()함수를 호출한다.
+    def dispatch(self, request, *args, **kwargs):
+        print("request.method:", request.method)
+        print("request.body:", request.body) # logger를 사용하는 것을 권장
+        print("request.POST:", request.POST)
+
+        return super().dispatch(request, *args, **kwargs)
+
+
 
