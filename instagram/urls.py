3a6dbfca6a138d1f18874776cc08f7a7a5b7fd1f
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2022-01-02 22:18:36 +0900

    인스타그램 ModelViewSet

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register("post", views.PostViewSet) # 이 시점에 2개의 URL을 만들어준다.
+# router.urls에 리스트 형태로 존재한다. url pattern list
+
 
 # django urls에서 urlpatterns를 요구하기 때문에 반드시 있어야함.
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all() # 데이터의 범위 지정, 로직마다 달라질 경우 get_queryset 사용가능
+    serializer_class = PostSerializer
+
+    # 이것만으로 post_list의 2개 분기, post_detail의 3개 분기를 지원한다. -> url이 2개 필요
+    #
+
