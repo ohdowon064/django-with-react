631f52dfeb634b1824afb5fcc6b3c6a7238c52a4
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2022-01-10 23:05:08 +0900

    serializer와 유효성 검사

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -2,37 +2,43 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Post
 
-
-# 모든 프로그래밍 언어의 통신에서 데이터는 반드시 바이트문자열로 표현되어야한다.
-# 송신자: 객체를 문자열로 변환하여 데이터 전송 -> 직렬화
-# 수신자: 수신한 문자열을 다시 객체로 변환하여 활용 -> 비직열화
-
-# Form 생성자의 첫 인자는 data, Serializer 생성자의 첫번째 인자는 instance(=Model.objects.first() 등으로 가져오는 객체들)이다.
-# Form처리와 유사한 방식으로 동작
-
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ["username", "email"]
 
 class PostSerializer(serializers.ModelSerializer):
-    # username = serializers.ReadOnlyField(source="author.username")
-    #
-    # class Meta:
-    #     model = Post
-    #     fields = [
-    #         "pk",
-    #         "username",
-    #         "message",
-    #         "created_at",
-    #         "updated_at"
-    #     ]
-
-    # author = AuthorSerializer()
-
     author_username = serializers.ReadOnlyField(source="author.username")
 
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = [
+            "pk",
+            "author_username",
+            "message",
+            "created_at",
+            "updated_at",
+            "is_public",
+            "ip"
+        ]
 
+
+# serializer 생성자에서 data=인자가 주어지면
+# .is_valid()가 호출된 후
+# .initial_data 필드에 접근가능
+# .validated_data를 통해 유효성 검증에 토과한 값들이 .save()에 사용된다.
+# .errors -> 유효성 검증 수행 후 오류내역
+# .data -> 유효성 검증 후 갱신된 인스턴스에 대한 필드값 사전
+
+# serializer 목적
+# 1. 직렬화
+# 2. 입력값에 대한 유효성 검증 후 디비 저장
+
+# 직렬화의 경우
+# 생성자의 instance 파라미터에 모델객체를 넘겨준다.
+
+# 유효성 검사
+# data 파라미터에 입력값을 넘겨준다.
+# 필드정의 시 validators를 지정 또는 클래스 Meta.validators를 지정
+# 필드레벨 -> validate_fieldname(self, value)
+# 오브젝트레벨 -> validate(self, data)
--- instagram/views.py
+++ instagram/views.py
@@ -9,6 +9,15 @@ from .models import Post
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all() # 데이터의 범위 지정, 로직마다 달라질 경우 get_queryset 사용가능
     serializer_class = PostSerializer
+    # authentication_classes =
+
+    def perform_create(self, serializer):
+        # FIXME: 인증이 되어있다는 가정하에, author를 지정정
+        author = self.request.user # 장고에서 모든 인증(토큰, 세션 등)의 중심에는 User모델이 존재한다.
+        # User or AnonymousUser
+        # 인증상황을 보장받아야한다.
+        ip = self.request.META['REMOTE_ADDR']
+        serializer.save(ip=ip, author=author)
 
     @action(detail=False, methods=["GET"])
     def public(self, request):
